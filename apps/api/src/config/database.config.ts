c8d0217324f4f8de2fcda1600ae19fec5efa69e2
masbouj <masbouj@gmail.com>
2024-11-12 22:15:38 +0100

    try connect db

--- apps/api/src/config/database.config.ts
+++ apps/api/src/config/database.config.ts
@@ -1,11 +1,23 @@
 import { ConfigService } from '@nestjs/config';
 import { SequelizeModuleOptions } from '@nestjs/sequelize';
 
-export const getDatabaseConfig = (
+function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export const getDatabaseConfig = async (
   configService: ConfigService,
-): SequelizeModuleOptions => ({
-  dialect: 'postgres',
-  uri: configService.get<string>('DATABASE_URL'),
-  autoLoadModels: true,
-  synchronize: true,
-});
+): Promise<SequelizeModuleOptions> => {
+  console.log('DATABASE_URL', process.env.DATABASE_URL);
+  console.log('DATABASE_URL 2', configService.get<string>('DATABASE_URL'));
+  console.log('NODE_ENV', process.env.NODE_ENV);
+
+  await sleep(2000); // pause de 2 secondes
+
+  return {
+    dialect: 'postgres',
+    uri: configService.get<string>('DATABASE_URL'),
+    autoLoadModels: true,
+    synchronize: true,
+  };
+};
