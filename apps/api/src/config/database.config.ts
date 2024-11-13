ab50306e0e55e439d2e023a3d540837368927237
masbouj <masbouj@gmail.com>
2024-11-13 21:16:45 +0100

    try build emv var

--- apps/api/src/config/database.config.ts
+++ apps/api/src/config/database.config.ts
@@ -1,18 +1,12 @@
 import { ConfigService } from '@nestjs/config';
 import { SequelizeModuleOptions } from '@nestjs/sequelize';
 
-/*function sleep(ms: number): Promise<void> {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}*/
-
 export const getDatabaseConfig = async (
   configService: ConfigService,
 ): Promise<SequelizeModuleOptions> => {
   console.log('DATABASE_URL', process.env.DATABASE_URL);
   console.log('DATABASE_URL 2', configService.get<string>('DATABASE_URL'));
   console.log('NODE_ENV', process.env.NODE_ENV);
-
-  //await sleep(2000); // pause de 2 secondes
 
   return {
     dialect: 'postgres',
