bb9d404cdadd839eb990625843c069870cce329e
masbouj <masbouj@gmail.com>
2024-11-14 22:43:36 +0100

    Update login.controller.ts

--- apps/api/src/modules/login/login.controller.ts
+++ apps/api/src/modules/login/login.controller.ts
@@ -17,18 +17,20 @@ import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('login')
 export class LoginController {
-  constructor(private readonly loginService: LoginService) { }
+  constructor(private readonly loginService: LoginService) {}
 
   @Post('/')
   @UsePipes(new ValidationPipe())
   async login(@Body() loginDto: LoginDto, @Res() res: Response) {
     const accessToken = await this.loginService.login(loginDto);
 
+    console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+
     res.cookie('token', accessToken, {
       httpOnly: true,
-      secure: false, //process.env.NODE_ENV === 'production', // Assure le cookie en HTTPS en production
-      maxAge: 60 * 24 * 60 * 60 * 1000, // 2 mois en millisecondes
-      sameSite: process.env.NODE_ENV === 'production' ? 'lax' : 'lax', // Empêche l'envoi du cookie sur des sites externes
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 60 * 24 * 60 * 60 * 1000, // 2 mois
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     });
 
     return res
